ccbddf2db8f9ef8ca7f4e90ce03b357b1637ac13
costinelnegura <pigeons-conduit0n@icloud.com>
2024-03-03 13:20:04 +0000

    updated to java 17, manually provide a ClientHttpConnector when building the WebClient

--- src/main/java/co/uk/negura/workshop_customer_api/util/ValidateTokenUtil.java
+++ src/main/java/co/uk/negura/workshop_customer_api/util/ValidateTokenUtil.java
@@ -3,9 +3,11 @@ package co.uk.negura.workshop_customer_api.util;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
 @Component
 public class ValidateTokenUtil {
@@ -13,8 +15,22 @@ public class ValidateTokenUtil {
     @Value("${auth.validateTokenApiUrl}")
     private String validateTokenApiUrl;
 
+    /*
+    Old way of creating WebClient, with java 11.
+     */
+//    public ResponseEntity<?> validateToken(String bearerToken) {
+//        WebClient webClient = WebClient.create();
+//        Mono<ResponseEntity<String>> responseEntityMono = webClient.get()
+//                .uri(validateTokenApiUrl)
+//                .header(HttpHeaders.AUTHORIZATION, "Bearer " + bearerToken)
+//                .exchangeToMono(response -> response.toEntity(String.class));
+//        return responseEntityMono.block();
+//    }
+
     public ResponseEntity<?> validateToken(String bearerToken) {
-        WebClient webClient = WebClient.create();
+        WebClient webClient = WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()))
+                .build();
         Mono<ResponseEntity<String>> responseEntityMono = webClient.get()
                 .uri(validateTokenApiUrl)
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + bearerToken)
